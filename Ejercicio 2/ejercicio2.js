fcac43455a1e8929eec203de0983579387cc17cd
Nahir Martinez <nahirmartinez09@gmail.com>
2023-05-17 17:54:00 -0300

    ejercicio 1 y 2 realizados

---
+++ Ejercicio 2/ejercicio2.js
@@ -0,0 +1,9 @@
+const h2Resultado = document.querySelector("#saludo-resultado");
+
+const SALUDOS_MAXIMOS = 5;
+
+for (let i = 0; i < SALUDOS_MAXIMOS ; i++) {
+ 
+    h2Resultado.innerHTML += (`hola <br>`);
+    
+}
